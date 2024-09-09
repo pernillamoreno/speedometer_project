496d9f2913d3cb27c92550fa81b95030d97fdc51
pernillamoreno <113672077+pernillamoreno@users.noreply.github.com>
2024-09-09 16:25:14 +0200

    Update tcpservice.h

--- av23-team-c/desktop/client/include/tcpservice.h
+++ av23-team-c/desktop/client/include/tcpservice.h
@@ -1,10 +1,30 @@
-#ifndef TCPCOM_H
-#define TCPCOM_H
+#ifndef TCPSERVICE_H
+#define TCPSERVICE_H
 
 #include "comservice.h"
+#include <thread>
+#include <atomic>
+#include <sys/socket.h>
 
 class TCPService : public COMService
 {
+    std::atomic<bool> running{true};
+    std::thread serviceThread{&TCPService::run, this};
+
+protected:
+    /**
+     * @brief Run the TCPService
+     *
+     */
+    void run() override;
+
+public:
+    TCPService() = default;
+    ~TCPService()
+    {
+        running = false;
+        serviceThread.join();
+    }
 };
 
-#endif
+#endif // TCPSERVICE_H
