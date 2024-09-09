9e53506d629f85488bca5dc15d256a4cd1da7cf7
pernillamoreno <113672077+pernillamoreno@users.noreply.github.com>
2024-09-09 16:32:15 +0200

    Update uartservice.h

--- av23-team-c/desktop/server/include/uartservice.h
+++ av23-team-c/desktop/server/include/uartservice.h
@@ -1,1 +1,32 @@
 
+#ifndef UARTCOM_H
+#define UARTCOM_H
+
+#include "comservice.h"
+#include <QSerialPort>
+#include <QThread>
+#include <atomic>
+
+class UARTService : public COMService, public QThread
+{
+    std::atomic<bool> runningUART{true};
+
+protected:
+    /**
+     * @brief Run the UARTService
+     *
+     */
+    void run() override;
+
+public:
+    UARTService()
+    {
+        start();
+    }
+    ~UARTService()
+    {
+        runningUART = false;
+
+        wait();
+    }
+};
