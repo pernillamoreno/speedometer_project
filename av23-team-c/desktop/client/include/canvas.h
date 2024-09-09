d252c8689b0db8e1ef6d8d13127b42a7a18dcd76
pernillamoreno <113672077+pernillamoreno@users.noreply.github.com>
2024-09-09 16:24:29 +0200

    Update canvas.h

--- av23-team-c/desktop/client/include/canvas.h
+++ av23-team-c/desktop/client/include/canvas.h
@@ -2,9 +2,53 @@
 #define CANVAS_H
 
 #include <QWidget>
+#include <QSoundEffect>
 
 class Canvas : public QWidget
 {
+
+public:
+    explicit Canvas(QWidget *parent = nullptr);
+
+    void setLeftArrow(bool value);
+    void setRightArrow(bool value);
+    void setWarning(bool value);
+    void setTemperature(int temp);
+    void setSpeed(int newSpeed);
+    void setBatteryLevel(int level);
+    void setConnectionStatus(bool connected);
+    void updateIndicators();
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+
+private:
+    void playSound();
+    void stopSound();
+    void updateSound();
+    void drawBackground(QPainter &painter);
+    void drawArc(QPainter &painter, const QPointF &center);
+    void drawCenterDots(QPainter &painter, const QPointF &center);
+    void drawNeedle(QPainter &painter, const QPointF &center);
+    void drawIcons(QPainter &painter, const QPointF &center);
+    void drawThermostat(QPainter &painter, const QPointF &center);
+    void drawConnectionStatus(QPainter &painter, const QPointF &center);
+    void drawBattery(QPainter &painter, const QPointF &center);
+    void drawSpeedometer(QPainter &painter, const QPointF &center);
+    void drawSpeedometerArc(QPainter &painter, QPoint center, int radius);
+    void drawSpeedometerLines(QPainter &painter, QPoint center, int radius);
+
+    bool isConnected;
+    bool leftArrowOn;
+    bool rightArrowOn;
+    bool warningOn;
+    bool warningVisible;
+    bool leftArrowVisible;
+    bool rightArrowVisible;
+    int temperature;
+    int batteryLevel;
+    int speed;
+    QSoundEffect soundEffect;
 };
 
-#endif
+#endif // CANVAS_H
