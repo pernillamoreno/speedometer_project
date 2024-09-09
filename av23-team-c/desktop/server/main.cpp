241027fa130597b9cf735ccf64edb0eddc327712
pernillamoreno <113672077+pernillamoreno@users.noreply.github.com>
2024-09-09 16:35:00 +0200

    Update main.cpp

--- av23-team-c/desktop/server/main.cpp
+++ av23-team-c/desktop/server/main.cpp
@@ -1,8 +1,23 @@
+#ifdef UARTCOM
+#include "uartservice.h"
+#else
+#include "tcpservice.h"
+#endif
+
+#include "window.h"
 #include <QApplication>
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
+
+#ifdef UARTCOM
+    UARTService service;
+#else
+    TCPService service;
+#endif
+    Window window(service);
+    window.show();
 
     return app.exec();
 }
