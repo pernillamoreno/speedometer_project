25867234a86862a3cb029217e29b7843cccc8fd8
pernillamoreno <113672077+pernillamoreno@users.noreply.github.com>
2024-09-09 16:27:15 +0200

    Update uartservice.h

--- av23-team-c/desktop/client/include/uartservice.h
+++ av23-team-c/desktop/client/include/uartservice.h
@@ -1,1 +1,27 @@
+#ifndef UARTSERVICE_H
+#define UARTSERVICE_H
 
+#include <QThread>
+#include <QSerialPort>
+#include <QByteArray>
+#include "comservice.h"
+#include "setting.h"
+
+class UARTService : public COMService, public QThread
+{
+public:
+    UARTService();
+    ~UARTService();
+
+protected:
+    /**
+     * @brief Run the UARTService
+     *
+     */
+    void run() override;
+
+private:
+    bool isRunning{true};
+};
+
+#endif
