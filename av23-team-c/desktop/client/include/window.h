0fc6d9dbf9a1d429dc6a75c0b9f5ee6ead2cb541
pernillamoreno <113672077+pernillamoreno@users.noreply.github.com>
2024-09-09 16:27:32 +0200

    Update window.h

--- av23-team-c/desktop/client/include/window.h
+++ av23-team-c/desktop/client/include/window.h
@@ -2,9 +2,25 @@
 #define WINDOW_H
 
 #include <QDialog>
+#include <QTimer>
+#include "canvas.h"
+#include "tcpservice.h"
 
 class Window : public QDialog
 {
+    COMService &comService;
+    Canvas canvas;
+    QTimer timer;
+    int counter{0};
+
+    /**
+     * @brief Used to redraw the canvas
+     *
+     */
+    void refresh();
+
+public:
+    Window(COMService &comService);
 };
 
 #endif
